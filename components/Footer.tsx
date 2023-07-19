86be4f13f433624190a6043197dfe6798e7c65bc
Yasin Yılmaz <yasinnyilmazz26@gmail.com>
2023-07-19 14:38:48 +0300

    ff

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -36,6 +36,17 @@ const Footer = () => {
           ))}
         </div>
       </div>
+      <div className="flex justify-between items-center flex-wrap mt-10 border-t border-gray-100 sm:px-16 px-6 py-10">
+        <div className="footer_copyrights-link">
+          <p>@2023 CarHub. All Rights Reserved </p>
+          <Link href="/" className="text-gray-500 px-5">
+            Privacy Policy
+          </Link>
+          <Link href="/" className="text-gray-500">
+            Terms of Use
+          </Link>
+        </div>
+      </div>
     </footer>
   );
 };
