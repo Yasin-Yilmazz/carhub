45d5944ca2caff8a825e46e0dec76147a2b0b86b
Yasin Yılmaz <yasinnyilmazz26@gmail.com>
2023-09-15 09:07:01 +0300

    added searchManufacturer

--- components/SearchBar.tsx
+++ components/SearchBar.tsx
@@ -1,9 +1,19 @@
 "use client";
+
+import { useState } from "react";
+import { SearchManufacturer } from "./";
+
 const SearchBar = () => {
+  const [manufacturer, setManufacturer] = useState("");
   const handleSearch = () => {};
   return (
     <form className="searchbar" onSubmit={handleSearch}>
-      <div className="searchbar__item"></div>
+      <div className="searchbar__item">
+        <SearchManufacturer
+          manufacturer={manufacturer}
+          setManufacturer={setManufacturer}
+        />
+      </div>
     </form>
   );
 };
